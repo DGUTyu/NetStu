8281b5e7bacbeda49e2bf6d67283a2b1915f4351
yu <2912435404@qq.com>
2022-10-20 23:24:36 +0800

    init project

---
+++ src/com/wxdgut/Test.java
@@ -0,0 +1,13 @@
+package com.wxdgut;
+
+/**
+ * @author Administrator
+ * @program: NetStu
+ * @date 2022-10-20 23:23:35
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test2022");
+    }
+}
